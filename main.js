b7736f9e16d62c9ec85d9d5029b3092f8d8f6f3e
Samyak Bhoyar <78425159+safdhuehyuyh@users.noreply.github.com>
2021-06-14 15:28:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -10,12 +15,24 @@ function setup(){
 }
 function draw(){
     background('#969A97');
+    document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+    fill('#f90093')
+    stroke('#f90093')
+    text("samyak");
 }
 function modelLoded(){
     console.log('PoseNet Is Initialized!')
 }
+
 function gotPoses(results){
-    if(results.lenth > 0){
+    if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + " noseY = " + noseY)
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference)
     }
 }
